99d401690ce88f0e39a6b048e2619452b207518f
Codemis <johnathan@pulos.com>
2021-08-25 17:25:36 -0700

    Added the default style of the SE Question Form.

--- wp-content/plugins/community-profile/includes/modules/SEQuestionForm/SEQuestionForm.php
+++ wp-content/plugins/community-profile/includes/modules/SEQuestionForm/SEQuestionForm.php
@@ -30,12 +30,53 @@ class COPR_SEQuestionForm extends ET_Builder_Module {
 	);
 
 	/**
+	 * A form template for each individual question
+	 *
+	 * @var string
+	 */
+	protected $formTemplate = '
+		<div class="copr-wrapper-$uniqueId$wrapperClasses" data-number="$questionNumber">
+			<div class="copr-question-field-wrapper">
+				<form action="$formAction" method="post">
+					<input type="hidden" name="action" value="copr_save_answer" />
+					<input type="hidden" name="tag" value="$tag" />
+					<input type="hidden" name="question_number" value="$questionNumber" />
+					<input type="hidden" name="question" value="$question" />
+					$nounce
+					<div class="form-element-wrapper">
+						<label>$questionNumber) $question</label>
+						$formElement
+					</div>
+					<div class="copr-form-buttons">
+						<div class="copr-width-50">
+							<a href="#" data-number="$questionNumber" class="copr-previous"><span class="dashicons dashicons-arrow-left-alt2"></span> $prevLabel</a> |
+							<a href="#" data-number="$questionNumber" class="copr-next">$nextLabel <span class="dashicons dashicons-arrow-right-alt2"></span></a>
+						</div>
+						<div class="copr-width-50 copr-align-right submit">
+							<input type="submit" name="submit" value="Save" />
+						</div>
+						<div class="copr-fixed"></div>
+					</div>
+				</form>
+			</div>
+		</div>';
+
+	/**
 	 * Initialize the module.
 	 *
 	 * @return void
 	 */
 	public function init() {
 		$this->name = esc_html__( 'SE Question Form', 'copr-community-profile' );
+	}
+
+	/**
+	 * Hide specific advanced fields
+	 *
+	 * @return array 	The fields to hide
+	 */
+	public function get_advanced_fields_config() {
+		return array();
 	}
 
 	/**
@@ -46,24 +87,24 @@ class COPR_SEQuestionForm extends ET_Builder_Module {
 	public function get_fields() {
 		return array(
 			'title'	=>	array(
-				'label'				=>	esc_html__('Section Title', 'myex-my-extension'),
+				'label'				=>	esc_html__('Section Title', 'copr-my-extension'),
 				'type'				=>	'text',
 				'option_category'	=>	'basic_option',
-				'description'		=>	esc_html__('The title for this section in the condition.', 'myex-my-extension'),
+				'description'		=>	esc_html__('The title for this section in the condition.', 'copr-my-extension'),
 				'toggle_slug'		=>	'main_content',
 			),
 			'tag'	=>	array(
-				'label'				=>	esc_html__('The Curriculum Tag (ie. C1-A)', 'myex-my-extension'),
+				'label'				=>	esc_html__('The Curriculum Tag (ie. C1-A)', 'copr-my-extension'),
 				'type'				=>	'text',
 				'option_category'	=>	'basic_option',
-				'description'		=>	esc_html__('The tag for this section.', 'myex-my-extension'),
+				'description'		=>	esc_html__('The tag for this section.', 'copr-my-extension'),
 				'toggle_slug'		=>	'main_content',
 			),
 			'questions'	=>	array(
-				'label'				=>	esc_html__('The Questions', 'myex-my-extension'),
+				'label'				=>	esc_html__('The Questions', 'copr-my-extension'),
 				'type'				=>	'textarea',
 				'option_category'	=>	'basic_option',
-				'description'		=>	esc_html__('Hit return after each question.', 'myex-my-extension'),
+				'description'		=>	esc_html__('Each question should be followed by a | and either the word text or scale.  Text will allow text answers.  Scale will provide a scale of options to choose from.', 'copr-my-extension'),
 				'toggle_slug'		=>	'main_content',
 			),
 		);
@@ -78,7 +119,45 @@ class COPR_SEQuestionForm extends ET_Builder_Module {
 	 * @return string              What to render
 	 */
 	public function render( $attrs, $content = null, $render_slug ) {
-		return sprintf( '<h1>%1$s</h1>', $this->props['content'] );
+		$lines = explode("<br />", $this->props['questions']);
+		$html = '';
+		$tag = strtolower(esc_html($this->props['tag']));
+		$nextLabel = esc_html__('Next', 'copr-my-extension');
+		$prevLabel = esc_html__('Previous', 'copr-my-extension');
+		foreach ($lines as $key => $line) {
+			$pieces = explode('|', $line);
+			if (count($pieces) < 2) {
+				/**
+				 * Missing details
+				 */
+				continue;
+			}
+			$formElement = '';
+			if (strtolower($pieces[1]) === 'text') {
+				$formElement = '<textarea name="answer" class="copr-answer-textarea" rows="10"></textarea>';
+			}
+			$question = $pieces[0];
+			$questionNumber = $key + 1;
+			$questionLabel = 'question-' . $tag . '-' . $questionNumber;
+			$wrapperClasses = '';
+			if ($key !== 0) {
+				$wrapperClasses = ' copr-hidden';
+			}
+			$vars = array(
+				'$formAction'		=>	admin_url('admin-ajax.php'),
+				'$formElement'		=>	$formElement,
+				'$nextLabel'		=>	$nextLabel,
+				'$nounce'			=>	wp_nonce_field('submit_answers'),
+				'$prevLabel'		=>	$prevLabel,
+				'$question'			=>	$question,
+				'$questionNumber'	=>	$questionNumber,
+				'$tag'				=>	$tag,
+				'$uniqueId'			=>	$questionLabel,
+				'$wrapperClasses'	=>	$wrapperClasses,
+			);
+			$html .= strtr($this->formTemplate, $vars);
+		}
+		return '<div class="copr-questions-wrapper">' . $html . '</div>';
 	}
 }
 
--- wp-content/plugins/community-profile/scripts/frontend.js
+++ wp-content/plugins/community-profile/scripts/frontend.js
@@ -1,3 +1,26 @@
 // This script is loaded both on the frontend page and in the Visual Builder.
 
-jQuery(function($) {});
+jQuery(function($) {
+  if ($('.copr-questions-wrapper').length > 0) {
+    /**
+     * Do some prework
+     */
+    $('.copr-questions-wrapper').each(function() {
+      const wrapper = $(this);
+      const children = wrapper.children();
+      const displayed = children.not('.copr-hidden');
+      const current = displayed.attr('data-number');
+      const total = children.length;
+      wrapper.attr('data-current', current);
+      wrapper.attr('data-total', total);
+      if (current) {
+        // Disable previous
+        displayed.find('.copr-previous').first().addClass('copr-disabled');
+      }
+      if (current == total) {
+        // Disable next
+        displayed.find('.copr-next').first().addClass('copr-disabled');
+      }
+    });
+  }
+});
