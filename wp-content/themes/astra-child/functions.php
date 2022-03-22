5d03541deec57f5a17b9834f6eb811e527fd23a7
Codemis <johnathan@pulos.com>
2022-03-21 18:31:59 -0700

    Added memory to the facet tool.

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -14,10 +14,11 @@
 define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
 
 /**
- * Enqueue styles
+ * Enqueue styles & scripts
  */
 function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+	wp_enqueue_script( 'astra-child-theme-js', get_stylesheet_directory_uri() . '/child-application.js', array('jquery'), CHILD_THEME_ASTRA_CHILD_VERSION, true );
 }
 
 /**
